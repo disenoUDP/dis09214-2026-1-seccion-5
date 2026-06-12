bfd25f8df889cc0f8ea6c2f5a08540aaa3b73f75
Matías Serrano <88412501+misaaaaaa@users.noreply.github.com>
2026-06-12 15:05:24 -0400

    agregué movimiento
    Updated draw function to include background and dynamic text size.

--- clases/clase-10/avances-examen.js
+++ clases/clase-10/avances-examen.js
@@ -2,9 +2,11 @@ function setup() {
   createCanvas(400, 400);
   textSize(30);
   textAlign(CENTER,CENTER);
-  text("esto he avanzado", width/2,height/2)
 }
 
 function draw() {
-  //background(220);
+  background(220);
+  textSize(frameCount);
+  text("esto he avanzado", width/2,height/2)
+
 }

858d3e44477286e677a96f396fd34e0d30d8003a
Matías Serrano <88412501+misaaaaaa@users.noreply.github.com>
2026-06-12 15:08:39 -0400

    añadir fecha de avance
    Refactor setup and draw functions for clarity and consistency.

--- clases/clase-10/avances-examen.js
+++ clases/clase-10/avances-examen.js
@@ -1,3 +1,21 @@
+// avance de hoy viernes 12 de junio
+
+function setup() {
+  createCanvas(400, 400);
+  textSize(30);
+  textAlign(CENTER,CENTER);
+}
+
+function draw() {
+  background(220);
+  textSize(frameCount);
+  text("esto he avanzado", width/2,height/2)
+
+}
+
+
+// avance de hoy domingo 14 de junio 
+
 function setup() {
   createCanvas(400, 400);
   textSize(30);

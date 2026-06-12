4041ef624c6cdf73bfc0a11fe92cd15641b7a8ab
Matías Serrano <88412501+misaaaaaa@users.noreply.github.com>
2026-06-12 15:04:00 -0400

    mis avances de hoy

---
+++ clases/clase-10/avances-examen.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(400, 400);
+  textSize(30);
+  textAlign(CENTER,CENTER);
+  text("esto he avanzado", width/2,height/2)
+}
+
+function draw() {
+  //background(220);
+}
